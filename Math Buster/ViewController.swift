566cab4f6c88c89a3c38875e2b1d49104844593d
9901kp <kaiypovgg@gmail.com>
2024-04-18 00:24:19 +0400

    Initial Commit

---
+++ Math Buster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Math Buster
+//
+//  Created by Мухаммед Каипов on 18/4/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
